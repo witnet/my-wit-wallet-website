ba877f5998015e126839bf14d421f1a8cf9b6c41
aeweda <aeweda@users.noreply.github.com>
2023-05-23 18:48:41 +0000

    deploy: 0612d7ad8862ac52f8cae0574020ea953934a385

--- getBrowserOs.ts
+++
@@ -1,7 +0,0 @@
-import { DEFAULT_OS } from './constants'
-
-export function getBrowserOs(navigator: any) {
-  const supportedOs = ['Win', 'Mac', 'Linux', 'MacIntel']
-  const platform = navigator.platform
-  return supportedOs.find((os) => platform.includes(os)) || DEFAULT_OS
-}
