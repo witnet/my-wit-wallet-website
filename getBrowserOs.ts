0a7cd120206845e84b7a5916578727cf4c521178
gabaldon <maria@otherplane.com>
2024-01-26 10:33:12 +0100

    fix: support windows

--- getBrowserOs.ts
+++ getBrowserOs.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_OS } from './constants'
 
 export function getBrowserOs(navigator: any) {
-  const supportedOs = ['Mac', 'Linux', 'MacIntel']
+  const supportedOs = ['Win', 'Mac', 'Linux', 'MacIntel']
   const platform = navigator.platform
   return supportedOs.find((os) => platform.includes(os)) || DEFAULT_OS
 }
