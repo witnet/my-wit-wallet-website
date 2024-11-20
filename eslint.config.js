dcc7becfe1afd3d95d0be10da3a131c89baca59f
gabaldon <maria@otherplane.com>
2024-11-20 13:26:53 +0100

    fix: add .output to eslint and prettier ignore

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,29 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import eslintConfigPrettier from "eslint-config-prettier";
-// eslint.config.js
-export default [
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+export default createConfigForNuxt().prepend(
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+  ...pluginVue.configs["flat/strongly-recommended"],
   eslintConfigPrettier,
   {
-    ignores: ['node_modules', 'dist', 'public', '.nuxt', 'docs/**/*', 'DEBUG=1/**/*'],
+    ignores: ['node_modules', 'dist', 'public', '.nuxt', 'docs/**/*', 'DEBUG=1/**/*', '.output', 'docs/_nuxt/*'],
   },
   {
     files: ['pages/*.vue'],
     rules: {
       'vue/multi-word-component-names': 0,
+      "vue/html-self-closing": ["error", {
+        "html": {
+          "void": "always",
+          "normal": "always",
+          "component": "always"
+        },
+        "svg": "always",
+        "math": "always"
+      }]
     },
   },
   {
@@ -26,9 +36,12 @@ export default [
         ...globals.node
       },
       parserOptions: {
+        parser: {
+          ts: tseslint.parser
+        },
         ecmaVersion: 2020,
         sourceType: 'module',
       },
     },
   },
-]
+)
